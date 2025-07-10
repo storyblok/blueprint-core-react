0d211062317a419e8389da1ba955f6ee707b94e1
Ronny Shani <63248335+ironnysh@users.noreply.github.com>
2025-07-10 12:51:44 +0200

    feat: add <footer>

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { StoryblokComponent, useStoryblok } from '@storyblok/react';
 import { useParams } from 'react-router';
 
 export default function App() {
+	const currentYear = new Date().getFullYear();
 	const { '*': slug } = useParams();
 	const story = useStoryblok(slug || 'home', {
 		version: 'draft',
@@ -9,5 +10,10 @@ export default function App() {
 	if (!story?.content) {
 		return <div>Loading...</div>;
 	}
-	return <StoryblokComponent blok={story.content} />;
+	return (
+		<>
+			<StoryblokComponent blok={story.content} />
+			<footer>All rights reserved © {currentYear}</footer>
+		</>
+	);
 }
